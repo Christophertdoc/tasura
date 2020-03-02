9fbdaecde97a55fa6f8854d573b2486f535fc696
Christopher Dougherty <christophertdoc@gmail>
2020-03-01 19:12:31 -0500

    cleanup

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import { ApolloProvider } from "@apollo/react-hooks"
 import { ApolloClient, HttpLink, InMemoryCache } from "apollo-boost"
 import { setContext } from "apollo-link-context"
 import Data from "./components/Data"
-// import { useQuery } from "@apollo/react-hooks"
-// import { gql } from "apollo-boost"
-// import { withApollo } from "@apollo/react-hoc"
 
-// export const USERS = gql`
-//   {  
-//     users {
-//       id 
-//       name
-//     }
-//   }  
-// `
 
 function App() {
 
@@ -28,16 +17,9 @@ function App() {
         cache: new InMemoryCache()
     })
 
-    // const { loading, error, data } = useQuery(USERS)
-    // if (loading) return "Loading..."
-    // if (error) return `Error! ${error.message}`
-
     return (
         <ApolloProvider client={client}>
             <h1>Title</h1>
-            {/* {data.users.map(user => (
-                <h2 key={user.id}>{user.name}</h2>
-            ))} */}
             <Data />
         </ApolloProvider>
     )
